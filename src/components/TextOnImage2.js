3ea40603f39b9ce9d9316a004f9fb0508fba6636
Hamzanaeem00 <hamzanaeemkhan977@gmail.com>
2023-03-20 13:21:37 +0500

    upload image

--- src/components/TextOnImage2.js
+++ src/components/TextOnImage2.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Dowloadpdf from './Dowloadpdf'
 
 
@@ -5,11 +6,22 @@ import Dowloadpdf from './Dowloadpdf'
 const TextOnImage2 = ({id, data }) => {
     console.log(id , data );
 
+    const [file, setFile] = useState();
+
+
+
+  const handleChange =(e)=>{
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
   return (
     <>
+            <input type="file" onChange={handleChange} />
     <div id={id} >
         <div>
-    <img src='/logo.png' alt={`${data.name}.certificate`}/>
+    {/* <img src='/logo.png' alt={`${data.name}.certificate`}/> */}
+            <img src={file} />
     </div>
     {data?.map((item)=>{
         console.log(item);
